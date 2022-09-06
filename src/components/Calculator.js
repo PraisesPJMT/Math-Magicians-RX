2c7cf9e57fbe67d86ac3b00af908f362eea01f32
PraisesPJMT <praisesmusa@outlook.com>
2022-09-06 09:40:24 +0100

    Add number components

---
+++ src/components/Calculator.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Numbers = () => (
+  <>
+    <span className="number light number-0">
+      0
+    </span>
+    <span className="number light number-1">
+      1
+    </span>
+    <span className="number light number-2">
+      2
+    </span>
+    <span className="number light number-3">
+      3
+    </span>
+    <span className="number light number-4">
+      4
+    </span>
+    <span className="number light number-5">
+      5
+    </span>
+    <span className="number light number-6">
+      6
+    </span>
+    <span className="number light number-7">
+      7
+    </span>
+    <span className="number light number-8">
+      8
+    </span>
+    <span className="number light number-9">
+      9
+    </span>
+  </>
+);
+
+
